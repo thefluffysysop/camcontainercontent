f583bccbd06faab503d005180ee032d12ebdc710
thefluffysysop <kloosterhuispro@gmail.com>
2019-03-18 15:59:28 +0100

    Small Text Edit

--- vsanreport1/vsanreport1.ps1
+++ vsanreport1/vsanreport1.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 Write-Output "----------------------------------------------------------------------------"
-Write-Output "CAMDEMO - Executing a LOCAL PS script using CAM|CONTAINER!"
+Write-Output "DEMO - Executing a LOCAL PS script using CAM|CONTAINER!"
 Write-Output "This script runs from INSIDE the CAM|CONTAINER but uses REMOTE config to feed the script"
 Write-Output "For example, this script will now connect to vCenter, "
 Write-Output "using variables for credentials and vCenter FQDN, that it will get "
