fb2cb8b12f6994ab2a5aa100cbcb4ec2af217dd6
thefluffysysop <kloosterhuispro@gmail.com>
2019-03-18 15:49:27 +0100

    Added first real content to vsanreport scriot, including explanation of how it runs, and the local execution variable for powershell

--- vsanreport1/vsanreport1.ps1
+++ vsanreport1/vsanreport1.ps1
@@ -1,1 +1,11 @@
-connect-viserver vcenter1.flufflap.local
+#!/usr/bin/env pwsh
+Write-Output "----------------------------------------------------------------------------"
+Write-Output "CAMDEMO - Executing a LOCAL PS script using CAM|CONTAINER!"
+Write-Output "This script runs from INSIDE the CAM|CONTAINER but uses REMOTE config to feed the script"
+Write-Output "For example, this script will now connect to vCenter, "
+Write-Output "using variables for credentials and vCenter FQDN, that it will get "
+Write-Output "from the file /remotecontent/localparameters/localparameters.ps1"
+Write-Output "This 'localparameters' file could be different in every location the container is run"
+Write-Output "----------------------------------------------------------------------------"
+get-command connect-viserver vcenter1.flufflap.local
+
